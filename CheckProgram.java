1790610cf77aa2a39bc2cc43f0c43d73724d8517
Nick Arthur <narthur157@gmail.com>
2015-11-21 16:56:39 +0100

    Func names working

--- CheckProgram.java
+++ CheckProgram.java
@@ -1,12 +1,21 @@
 import CPP.Absyn.*;
 
-public class CheckProgram implements Program.Visitor<Env,Env> {
+public class CheckProgram implements 
+	Program.Visitor<Env,Env>,
+	Def.Visitor<Env, Env> {
 	public Env visit(PDefs p, Env env) {
 		System.out.println("Visiting your program yoooo");
-//		for (Def x : p.listdef_) {
+		
+		for (Def x : p.listdef_) {
 			//p.listdef_.add(x.accept(this,env));
-//			x.accept(this, env);
-//		}
+			x.accept(this, env);
+		}
 		return env;//return new CPP.Absyn.PDefs(listdef_);
+	}
+	
+	public Env visit(CPP.Absyn.DFun p, Env env) {
+		//env.
+		System.out.println("found function name: " + p.id_);
+		return env;
 	}
 }
--- TypeChecker.java
+++ TypeChecker.java
@@ -6,19 +6,17 @@ public class TypeChecker {
 	
 	public void typecheck(Program p) {
 		Env env = new Env();
-		
-		
 		p.accept(new CheckProgram(), env);
 	}
 	
-	public static class ProgramImpl extends Program {
-        
-        @Override
-		public <R,A> R accept(Program.Visitor<R, A> v, A env) {
-			System.out.println("???");
-			return null;
-			//return v.visit( this, env );
-		}
-	
-    }
+//	public static class ProgramImpl extends Program {
+//        
+//        @Override
+//		public <R,A> R accept(Program.Visitor<R, A> v, A env) {
+//			System.out.println("???");
+//			return null;
+//			//return v.visit( this, env );
+//		}
+//	
+//    }
 }
