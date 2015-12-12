1e7276e43ca643032028a6d0d464cca99834441c
Nick Arthur <narthur157@gmail.com>
2015-12-12 16:38:40 +0100

    again

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class CodeGenerator implements
 	Program.Visitor<Object, Object>, 
 	Def.Visitor<Object, Object>,
-	Arg.Visitor<Object, Object>,
+	Arg.Visitor<int, Object>,
 	Stm.Visitor<Object, Object>,
     Exp.Visitor<Object, Object> {
 		
@@ -24,7 +24,7 @@ public class CodeGenerator implements
     private void writeBoilerplate() {
         ct.writeInstr(".class public" + fileName);
         ct.writeInstr(".super java/lang/Object");
-        ct.startMethod("<init>()V");
+        ct.startMethod("<init>()V", true);
         ct.writeInstr("aload_0");
         ct.writeInstr("invokespecial java/lang/Object/<init>()V");
         ct.writeInstr("return");
@@ -41,22 +41,89 @@ public class CodeGenerator implements
 
         return null;
     }
+
+    public String jasminType(int typeCode) {
+        if (typeCode == TypeCode.CInt) {
+            return  "I";
+        }
+        else if (typeCode == TypeCode.CBool) {
+            return  "B";
+        }
+        else if (typeCode == TypeCode.CVoid) {
+            return  "V";
+        }
+        else {
+            return "Invalid type";
+        }
+    }
     
     /* FUNCTIONS */
     public Object visit(DFun p, Object unused) { 
-        ct.startMethod(p.id_ )                
+        // Make the function signature
+        String argDescriptor = "(";
+
+        if (p.id_.equals("main")) {
+            argDescriptor = "([Ljava/lang/String;)V";
+        } 
+        else {
+            for (Arg a : p.listarg_) {
+                int typeCode = a.accept(this, unused);
+                argDescriptor += jasminType(typeCode);
+            }
+
+            argDescriptor += ")" + jasminType(p.type_.accept(new TypeCode(), unused));
+        }
+
+        ct.startMethod(p.id_ + argDescriptor);               
+
+        // Set up the stack
+
+        ct.writeInstr(".limit stack " + p.liststm_.size());
+        ct.writeInstr(".limit locals 100");
+
+        for (Stm s : p.liststm_) {
+            s.accept(this, unused);
+        }
+
+        ct.endMethod();
+    
         return null; 
     }
     
     /* ARGS */
-    public Object visit(ADecl p, Object unused) { return null; }
+    public int visit(ADecl p, Object unused) { 
+        ct.addVar(p.id_);
+        return null; 
+    }
     
     /* STATEMENTS */
-    public Object visit(SExp p, Object unused) { return null; }
-    public Object visit(SDecls p, Object unused) { return null; }
-    public Object visit(SInit p, Object unused) { return null; }
-    public Object visit(SReturn p, Object unused) { return null; }
-    public Object visit(SWhile p, Object unused) { return null; }
+    public Object visit(SExp p, Object unused) { 
+        p.exp_.accept(this, unused);
+        return null; 
+    }
+    public Object visit(SDecls p, Object unused) { 
+        // don't push on to stack (??)
+        for (String s : p.listid_) {
+            ct.addVar(s);
+        }
+        return null; 
+    }
+    public Object visit(SInit p, Object unused) { 
+        ct.addVar(p.id_);
+
+        p.accept(p.exp_, unused);
+
+        return null; 
+    }
+    public Object visit(SReturn p, Object unused) { 
+        p.exp_.accept(this, unused);
+        ct.writeInstr("ireturn");
+        return null; 
+    }
+    public Object visit(SWhile p, Object unused) { 
+             
+        return null; 
+    }
     public Object visit(SBlock p, Object unused) { return null; }
     public Object visit(SIfElse p, Object unused) { return null; }
     
